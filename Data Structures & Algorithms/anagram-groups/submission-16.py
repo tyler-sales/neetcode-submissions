aafe22df9b1b80a11105c346780784add2adafcc
Tyler Sales <tylerdsales@berkeley.edu>
2026-07-12 18:56:53 -0700

    Add: anagram-groups - submission-16

---
+++ Data Structures & Algorithms/anagram-groups/submission-16.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+        final = []
+        
+        for word in strs:
+            order = "".join(sorted(word))
+            if anagrams.get(order) != None:
+                anagrams.get(order).append(word)
+            else:
+                anagrams[order] = [word]
+        
+        for words in anagrams.values():
+            final.append(words)
+        return final
+            
