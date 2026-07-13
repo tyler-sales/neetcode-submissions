a36844420f368bdc7f2a0384fe815111d84c2a10
Tyler Sales <tylerdsales@berkeley.edu>
2026-07-12 22:30:15 -0700

    Add: anagram-groups - submission-27

---
+++ Data Structures & Algorithms/anagram-groups/submission-27.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+
+        for word in strs:
+            count = [0] * 26
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            anagrams[tuple(count)].append(word)
+        
+        return list(anagrams.values())
+            
