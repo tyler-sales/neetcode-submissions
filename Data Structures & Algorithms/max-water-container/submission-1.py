77544d3d9c9136b8fa6b4405f13d3452ad2d4d81
Tyler Sales <tylerdsales@berkeley.edu>
2026-09-03 11:59:22 -0700

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # two pointers on opposite sides
+        # iterating through the list
+        # finding the min and the distance to calculate the amount of water
+        # keeping track of the highest amount
+        # either going until pointers cross or impossible to maximize more <- final step efficency gain
+
+        l, r, w = 0, len(heights) - 1, len(heights) - 1
+        maximum = 0
+
+        while l < r:
+            if heights[l] < heights[r]:
+                maximum = max(heights[l] * w, maximum)
+                l += 1
+            else:
+                maximum = max(heights[r] * w, maximum)
+                r -= 1
+            w -= 1
+        
+        return maximum
+
+            
+        
