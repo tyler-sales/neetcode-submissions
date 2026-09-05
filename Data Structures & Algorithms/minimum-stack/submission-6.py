588e83620734a2e4de0ccc7103eadcf0879868e0
Tyler Sales <tylerdsales@berkeley.edu>
2026-09-04 19:23:01 -0700

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -0,0 +1,28 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+        # sorted way
+        # having a definitive minimum  that is set with each push
+        # would need to update with a pop
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minstack == []:
+            self.minstack.append(val)
+        else:
+            self.minstack.append(min(val, self.minstack[-1]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minstack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
+        
